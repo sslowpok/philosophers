fac93ac03a29d2c10edd6fbc9e9405e6996dfb3d
Sheryl Slowpoke <sslowpok@su-d5.msk.21-school.ru>
2022-04-09 12:39:40 +0300

    created philos

--- philo/src/input.c
+++ philo/src/input.c
@@ -6,19 +6,30 @@
 /*   By: sslowpok <sslowpok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 15:22:54 by sslowpok          #+#    #+#             */
-/*   Updated: 2022/04/07 15:46:28 by sslowpok         ###   ########.fr       */
+/*   Updated: 2022/04/09 12:09:43 by sslowpok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	input(int argc, char **argv, t_philo *philo)
+void	input(int argc, char **argv, t_info *info)
 {
-	philo->args.count = ft_atoi(argv[1]);
-	philo->args.time_to_die = ft_atoi(argv[2]);
-	philo->args.time_to_eat = ft_atoi(argv[3]);
-	philo->args.time_to_sleep = ft_atoi(argv[4]);
-	philo->args.num_of_eat= -1;
+	// philo->args.count = ft_atoi(argv[1]);
+	// philo->args.time_to_die = ft_atoi(argv[2]);
+	// philo->args.time_to_eat = ft_atoi(argv[3]);
+	// philo->args.time_to_sleep = ft_atoi(argv[4]);
+	// if (argc == 6)
+	// 	philo->args.num_of_eat = ft_atoi(argv[5]);
+	// else if (argc == 5)
+	// 	philo->args.num_of_eat= -1;
+
+
+	info->num = ft_atoi(argv[1]);
+	info->t2die = ft_atoi(argv[2]);
+	info->t2eat = ft_atoi(argv[3]);
+	info->t2sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-		philo->args.num_of_eat = ft_atoi(argv[5]);
+		info->num_of_meals = ft_atoi(argv[5]);
+	else
+		info->num_of_meals = -1;
 }
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,20 +6,52 @@
 /*   By: sslowpok <sslowpok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 09:58:35 by alex              #+#    #+#             */
-/*   Updated: 2022/04/07 15:22:23 by sslowpok         ###   ########.fr       */
+/*   Updated: 2022/04/09 12:38:02 by sslowpok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+void	*routine(void *data)
+{
+	(void)	data;
+	printf("Philo created!\n");
+	return ((void *)0);
+}
+
+int	init_philos(t_philo *philos, int num)
+{
+	int	i;
+
+	philos = malloc(sizeof(t_philo) * num);
+	if (!philos)
+		return (0);
+	i = -1;
+	while (++i < num)
+	{
+		philos[i].id = pthread_create(&philos[i].thread, NULL, routine, philos + i);
+		if (philos[i].id != 0)
+		{
+			printf("Thread create error\n");
+			return (0);
+		}
+		// pthread_join(philos[i].thread, 0);
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo	philo;
+	t_info	info;
 
 	if (!check_args(argc, argv))
 		return (1);
-	input(argc, argv, &philo);
+	input(argc, argv, &info);
+	if (!init_philos(info.philos, info.num))
+		return (1);
 	
-	
+
+
+
 	return (0);
 }
